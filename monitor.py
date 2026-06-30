866ae4908ca78be2b75683f63ed810248d2d0d53
WACEEREE <100634892+WACEEREE@users.noreply.github.com>
2026-06-30 22:38:18 +0100

    Add files via upload

---
+++ monitor.py
@@ -0,0 +1,48 @@
+import requests
+import urllib3
+from bs4 import BeautifulSoup
+import time
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+TOKEN = "8788275730:AAHcUEILxqypngK_-Ojfvv75x1ctUm36KcE"
+CHAT_ID = "1704738927"
+
+URL = "https://drh.sante.gov.ma/Pages/Concours_Ex_D_ParaMedical_D.aspx"
+
+old_links = []
+first_run = True
+
+while True:
+    print("جاري الفحص...")
+
+    html = requests.get(URL, verify=False).text
+    soup = BeautifulSoup(html, "html.parser")
+
+    for a in soup.find_all("a", href=True):
+
+        text = a.get_text(strip=True)
+
+        if "Liste" in text or "Résultat" in text:
+
+            link = requests.compat.urljoin(URL, a["href"])
+
+            if link not in old_links:
+
+                old_links.append(link)
+
+                if not first_run:
+
+                    requests.get(
+                        f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+                        params={
+                            "chat_id": CHAT_ID,
+                            "text": f"📢 جديد!\n\n{text}\n{link}"
+                        }
+                    )
+
+                    print("✅ تم إرسال إشعار")
+
+    first_run = False
+
+    time.sleep(300)   # يفحص كل 5 دقائق
